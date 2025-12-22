dffc0479087ef46004723e6e1175085bd9c03f68
bala-amypo <bala@amypo.in>
2025-12-22 03:38:27 +0000

    User commit

--- src/main/java/com/example/demo/repository/GuestRepository.java
+++ src/main/java/com/example/demo/repository/GuestRepository.java
@@ -3,5 +3,5 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.Guest;
 
-public interface GuestRepository extends JpaRepository<Guest, Long> {
+ interface GuestRepository extends JpaRepository<Guest, Long> {
 }
--- src/main/java/com/example/demo/service/ GuestService.java
+++ src/main/java/com/example/demo/service/ GuestService.java
@@ -3,7 +3,7 @@ package com.example.demo.service;
 import com.example.demo.model.Guest;
 import java.util.List;
 
-public interface GuestService {
+interface GuestService {
     Guest createGuest(Guest guest);
     Guest updateGuest(Long id, Guest guest);
     Guest getGuestById(Long id);
