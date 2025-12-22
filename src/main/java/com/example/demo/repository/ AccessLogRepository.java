06714b1075e8f20b3fe99ed51aa5b189a6b8f86c
bala-amypo <bala@amypo.in>
2025-12-22 02:59:09 +0000

    User commit

--- src/main/java/com/example/demo/repository/ AccessLogRepository.java
+++ src/main/java/com/example/demo/repository/ AccessLogRepository.java
@@ -3,7 +3,6 @@ package com.example.demo.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.AccessLog;
 import java.util.List;
-
 public interface AccessLogRepository extends JpaRepository<AccessLog, Long> {
     List<AccessLog> findByGuestId(Long guestId);
     List<AccessLog> findByDigitalKeyId(Long keyId);
